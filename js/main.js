d3c421666380de7e9abf41ab4912055558565f5e
webify-io <webify.projects@gmail.com>
2024-03-14 16:09:28 +0200

    merged type.js in main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/* js for library from the CDN for typed.js from [https://github.com/mattboldt/typed.js] */
+var typed = new Typed('.text', {
+	strings: ['Websites', 'Applications', 'Software', 'Development'],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true,
+});
+
 /* ========== Sticky Header: Disabled when the viewport is 450px or less. =========== */
 let headers = document.querySelectorAll('header');
 
--- js/typed.js
+++
@@ -1,8 +0,0 @@
-/* js for library from the CDN for typed.js from [https://github.com/mattboldt/typed.js] */
-var typed = new Typed('.text', {
-	strings: ['Websites', 'Applications', 'Software', 'Development'],
-	typeSpeed: 100,
-	backSpeed: 100,
-	backDelay: 1000,
-	loop: true,
-});
