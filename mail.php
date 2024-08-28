186450d9f6989a9edc346ad14f5caa627a5f7b97
webify-io <webify.projects@gmail.com>
2024-08-28 15:47:38 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -14,7 +14,7 @@ if(mail($recipient, $subject, $message, $mailheader)) {
     // Send a confirmation email to the user
     $user_subject = "Thank you for contacting us!";
     $user_message = "Dear $name,\n\nThank you for reaching out to us. We have received your message and will get back to you shortly.\n\nBest regards,\nWebify Team";
-    $user_mailheader = 'From: info@webify.org.za\r\n';
+    $user_mailheader = 'From: info@webify.org.za';
 
     mail($email, $user_subject, $user_message, $user_mailheader);
 
